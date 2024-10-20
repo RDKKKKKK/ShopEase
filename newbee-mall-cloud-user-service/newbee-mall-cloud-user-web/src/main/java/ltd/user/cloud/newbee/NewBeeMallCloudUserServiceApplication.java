26fe4f6d88c21085f5c7c2b5eda2edacb18f1881
roderick <1614818499@qq.com>
2024-10-20 17:45:04 +0800

    User service k8s done

--- newbee-mall-cloud-user-service/newbee-mall-cloud-user-web/src/main/java/ltd/user/cloud/newbee/NewBeeMallCloudUserServiceApplication.java
+++ newbee-mall-cloud-user-service/newbee-mall-cloud-user-web/src/main/java/ltd/user/cloud/newbee/NewBeeMallCloudUserServiceApplication.java
@@ -18,10 +18,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @MapperScan("ltd.user.cloud.newbee.dao")
 public class NewBeeMallCloudUserServiceApplication {
 
-
     public static void main(String[] args) {
         System.setProperty("nacos.logging.default.config.enabled","false");
         SpringApplication.run(NewBeeMallCloudUserServiceApplication.class, args);
     }
+
+
 
 }
