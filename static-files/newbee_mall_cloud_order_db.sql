5fbe061934179e8d7aabecdaafac167085f4b8ff
roderick <1614818499@qq.com>
2024-10-21 10:30:23 +0800

    Persistence architecture done

--- static-files/newbee_mall_cloud_order_db.sql
+++ static-files/newbee_mall_cloud_order_db.sql
@@ -12,7 +12,7 @@ CREATE TABLE `tb_newbee_mall_order` (
                                         `total_price` int(11) NOT NULL DEFAULT '1' COMMENT '订单总价',
                                         `pay_status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '支付状态:0.未支付,1.支付成功,-1:支付失败',
                                         `pay_type` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0.无 1.支付宝支付 2.微信支付',
-                                        `pay_time` timestamp DEFAULT NULL COMMENT '支付时间',
+                                        `pay_time` timestamp NULL DEFAULT NULL COMMENT '支付时间',  -- 修改了这里
                                         `order_status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '订单状态:0.待支付 1.已支付 2.配货完成 3:出库成功 4.交易成功 -1.手动关闭 -2.超时关闭 -3.商家关闭',
                                         `extra_info` varchar(100) NOT NULL DEFAULT '' COMMENT '订单body',
                                         `is_deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '删除标识字段(0-未删除 1-已删除)',
